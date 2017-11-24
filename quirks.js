fe0a6a2725f1d22747fb741d274c3dea0ca281b3
William <he.william@yahoo.com>
2017-11-24 10:39:28 -0800

    added quirks

---
+++ quirks.js
@@ -0,0 +1,15 @@
+var a = 1, b = 2, c;
+
+(function(){
+  const a = 7;
+  b = 200;
+  let d = 10;
+  var c = 5;
+})()
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+//mainly all scopes question
